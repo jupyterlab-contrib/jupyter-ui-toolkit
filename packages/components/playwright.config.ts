901060d3c2745b410796c58dd871b2e1c1cc7e7e
Frédéric Collonval <fcollonval@gmail.com>
2022-01-15 18:55:43 +0100

    Add visual test structure

---
+++ packages/components/playwright.config.ts
@@ -0,0 +1,34 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  testMatch: '**/*.test.ts',
+  webServer: {
+    command: 'yarn run start',
+    port: 6006,
+    timeout: 120 * 1000,
+    // It is safe to reuse the server for stories testing
+    reuseExistingServer: true
+  },
+  use: {
+    baseURL: process.env.TARGET_URL ?? 'http://localhost:6006',
+    trace: 'on-first-retry'
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
+    }
+  ]
+};
+
+export default config;
