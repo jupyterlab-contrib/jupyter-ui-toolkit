caba050e976acbb53181f5348feef983e578558b
Frédéric Collonval <fcollonval@gmail.com>
2023-06-28 17:51:47 +0200

    Some visual test fixes
    Fix format:check
    
    Fix packaging
    
    Update all snapshots

--- packages/components/src/menu/menu.test.ts
+++ packages/components/src/menu/menu.test.ts
@@ -6,7 +6,7 @@ import { test, expect } from '@playwright/test';
 test('Default', async ({ page }) => {
   await page.goto('/iframe.html?id=menu--default');
 
-  expect(await page.locator('#root > jp-menu').screenshot()).toMatchSnapshot(
-    'menu-default.png'
-  );
+  expect(
+    await page.locator('#storybook-root > jp-menu').screenshot()
+  ).toMatchSnapshot('menu-default.png');
 });
