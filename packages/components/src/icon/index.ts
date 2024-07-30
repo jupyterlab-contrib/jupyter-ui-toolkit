dab9928d81e6f242560a8dd5e9220d5bf70ccb75
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-07-30 21:05:52 +0530

    updated changes

--- packages/components/src/icon/index.ts
+++ packages/components/src/icon/index.ts
@@ -9,15 +9,7 @@ import {
   html
 } from '@microsoft/fast-element';
 
-const template = html<Icon>`
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    innerHTML="${x => x.getSvg()}"
-  ></svg>
-`;
+const template = html<Icon>`<div :innerHTML="${x => x.getSvg()}"></div>`;
 
 @customElement({
   name: 'jp-icon',
@@ -36,7 +28,7 @@ export class Icon extends FASTElement {
   }
 
   static defaultIcon(): string {
-    return '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/>';
+    return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" fill="currentColor"/></svg>';
   }
 
   getSvg(): string {
