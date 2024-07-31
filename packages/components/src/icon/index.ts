4be166b3dcc106d461b1713f5b03689489da892a
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-07-31 20:52:49 +0530

    added comments

--- packages/components/src/icon/index.ts
+++ packages/components/src/icon/index.ts
@@ -11,15 +11,33 @@ import {
 
 const template = html<Icon>`<div :innerHTML="${x => x.getSvg()}"></div>`;
 
+/**
+ * Icon component
+ *
+ * Icon must first be registered: `Icon.register({ name, svgStr });`
+ *
+ * Then you can use it with `<jp-icon name="{name}"></jp-icon>` .
+ *
+ * To style your icon, you should set `fill` and/or `stroke` attributes to `currentColor`.
+ * Then the icon will be colored with the active text color.
+ */
 @customElement({
   name: 'jp-icon',
   template
 })
 export class Icon extends FASTElement {
+  /**
+   * Name of the icon to display.
+   */
   @attr name: string;
 
   private static iconsMap = new Map<string, string>();
 
+  /**
+   *Register a new icon.
+   *
+   * @param options { name: Icon unique name, svgStr: Icon SVG as string }
+   */
   static register(options: { name: string; svgStr: string }): void {
     if (Icon.iconsMap.has(options.name)) {
       throw new Error(`Icon with name ${options.name} is already defined`);
@@ -27,10 +45,19 @@ export class Icon extends FASTElement {
     Icon.iconsMap.set(options.name, options.svgStr);
   }
 
+  /**
+   * Default icon
+   *
+   * This icon will be displayed if the {@link name} does not match any known
+   * icon names.
+   */
   static defaultIcon(): string {
     return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" fill="currentColor"/></svg>';
   }
 
+  /**
+   * Get the icon SVG
+   */
   getSvg(): string {
     return Icon.iconsMap.get(this.name) ?? Icon.defaultIcon();
   }
