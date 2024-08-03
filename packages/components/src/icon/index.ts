3153acbcf23e8248047ca27f5ca971be1e027a7a
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-08-03 12:30:10 +0530

    updated changes

--- packages/components/src/icon/index.ts
+++ packages/components/src/icon/index.ts
@@ -28,7 +28,8 @@ export class Icon extends FASTElement {
   @attr name: string;
 
   private static iconsMap = new Map<string, string>();
-  private static _defaultIcon: string = Icon.defaultIcon();
+  private static _defaultIcon =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" fill="currentColor"/></svg>';
 
   /**
    * Register a new icon.
@@ -51,9 +52,8 @@ export class Icon extends FASTElement {
     document
       .querySelectorAll(`jp-icon[name="${options.name}"]`)
       .forEach((node: HTMLElement) => {
-        const element = node as HTMLElement;
-        element.setAttribute('name', '');
-        element.setAttribute('name', options.name);
+        node.setAttribute('name', '');
+        node.setAttribute('name', options.name);
       });
   }
 
