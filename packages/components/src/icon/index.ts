2a07ebb1be76ad4b2af161015fcf84600491d80c
Frédéric Collonval <fcollonval@users.noreply.github.com>
2024-08-05 10:14:36 +0200

    Improve icon element DOM tree

--- packages/components/src/icon/index.ts
+++ packages/components/src/icon/index.ts
@@ -1,11 +1,5 @@
-import {
-  FASTElement,
-  customElement,
-  attr,
-  html
-} from '@microsoft/fast-element';
-
-const template = html<Icon>`<div :innerHTML="${x => x.getSvg()}"></div>`;
+import { FASTElement, customElement, attr } from '@microsoft/fast-element';
+import { iconStyles as styles } from './icon.styles';
 
 /**
  * Icon component
@@ -19,14 +13,9 @@ const template = html<Icon>`<div :innerHTML="${x => x.getSvg()}"></div>`;
  */
 @customElement({
   name: 'jp-icon',
-  template
+  styles
 })
 export class Icon extends FASTElement {
-  /**
-   * Name of the icon to display.
-   */
-  @attr name: string;
-
   private static iconsMap = new Map<string, string>();
   private static _defaultIcon =
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" fill="currentColor"/></svg>';
@@ -50,7 +39,7 @@ export class Icon extends FASTElement {
 
     // Rerender all existing icons with the same name
     document
-      .querySelectorAll(`jp-icon[name="${options.name}"]`)
+      ?.querySelectorAll(`jp-icon[name="${options.name}"]`)
       .forEach((node: HTMLElement) => {
         node.setAttribute('name', '');
         node.setAttribute('name', options.name);
@@ -77,9 +66,32 @@ export class Icon extends FASTElement {
   }
 
   /**
+   * Name of the icon to display.
+   */
+  @attr name: string;
+  nameChanged() {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = this.getSvg();
+    }
+  }
+
+  /**
+   * The connected callback for this FASTElement.
+   * @remarks
+   * This method is invoked by the platform whenever this FASTElement
+   * becomes connected to the document.
+   */
+  connectedCallback(): void {
+    super.connectedCallback();
+    this.nameChanged();
+  }
+
+  /**
    * Get the icon SVG
    */
   getSvg(): string {
     return Icon.iconsMap.get(this.name) ?? Icon.defaultIcon();
   }
 }
+
+export { styles as iconStyles };
